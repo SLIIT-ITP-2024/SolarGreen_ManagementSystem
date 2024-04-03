18d6c836aef2b591d765ceecd8ff8f0e7b405d5f
gamithf <125730055+gamithf@users.noreply.github.com>
2024-04-03 08:09:52 +0530

    Created the Projects table

---
+++ Client/src/components/InstallationManagement/Table/AllProjects.jsx
@@ -0,0 +1,48 @@
+function AllProjects() {
+    const styles = {
+        marginRight: "15px",
+    };
+
+    return (
+        
+        <div className="container">
+            <br></br>
+            <h3>Projects</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>PID</th>
+                        <th>Customer</th>
+                        <th>Type</th>
+                        <th>Status</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>P001</td>
+                        <td>xxx</td>
+                        <td>xxx</td>
+                        <td>xxx</td>
+                        <td>
+                            <button type="button" className="btn btn-warning" style={styles}>Update</button>
+                            <button type="button" className="btn btn-danger">Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>P002</td>
+                        <td>xxx</td>
+                        <td>xxx</td>
+                        <td>xxx</td>
+                        <td>
+                            <button type="button" className="btn btn-warning" style={styles}>Update</button>
+                            <button type="button" className="btn btn-danger">Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default AllProjects;
