b1cf63c047beaf94fc5319ce3093b3ae725ecdff
gamithf <125730055+gamithf@users.noreply.github.com>
2024-04-03 06:13:33 +0530

    Tested the page

--- Client/src/pages/InstallationManagement/InstallationManagementPage.jsx
+++ Client/src/pages/InstallationManagement/InstallationManagementPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import WithLayout from '../../hoc'
+import TestCom from '../../components/InstallationManagement/__tests__/TestCom'
 
 const InstallationManagementPage = () => {
   return (
     <div>
         <h1>Installation Management</h1>
-        //add your components here
+        <TestCom/>
     </div>
   )
 }
