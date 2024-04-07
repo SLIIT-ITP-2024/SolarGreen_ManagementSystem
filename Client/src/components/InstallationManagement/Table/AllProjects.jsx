61088074a72b7fdd09d092934188d70a0dfa5d6a
gamithf <125730055+gamithf@users.noreply.github.com>
2024-04-07 08:12:29 +0530

    Update AllProjects.jsx

--- Client/src/components/InstallationManagement/Table/AllProjects.jsx
+++ Client/src/components/InstallationManagement/Table/AllProjects.jsx
@@ -3,6 +3,41 @@ import { Link } from "react-router-dom";
 
 function AllProjects() {
 
+    const [projects, setProjects] = useState([]);
+
+    // Get all projects
+
+    // useEffect(() => {
+    //     function getProjects() {
+    //         axios.get("http://localhost:8070/project/")
+    //             .then((res) => {
+    //                 // console.log(res.data);
+    //                 setProjects(res.data);
+    //             })
+    //             .catch((err) => {
+    //                 alert(err.message);
+    //             });
+    //     }
+
+    //     getProjects();
+    // }, []);
+
+    // Delete a project
+    
+    // const handleDelete = async (id) => {
+    //     const confirmDelete = window.confirm("Are you sure you want to delete this record?");
+    //     if (confirmDelete) {
+    //         try {
+    //             await axios.delete(`http://localhost:8070/project/delete/${id}`);
+    //             // Remove the deleted student from the state
+    //             setProjects(projects.filter(project => project._id !== id));
+    //         } catch (error) {
+    //             console.error("Error deleting record:", error);
+    //             alert("An error occurred while deleting the record");
+    //         }
+    //     }
+    // };
+
     return (  
         <div className="container">
             <br></br>
