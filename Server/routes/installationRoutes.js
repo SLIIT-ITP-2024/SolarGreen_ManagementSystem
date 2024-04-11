eb11d69f30e815b87ec1da29de4255cf0cf624cf
gamithf <125730055+gamithf@users.noreply.github.com>
2024-04-11 07:33:43 +0530

    Updated the backend for projects

--- Server/routes/installationRoutes.js
+++ Server/routes/installationRoutes.js
@@ -1,10 +1,16 @@
-const installationController = require('../controllers/installationController');
 const express = require('express');
 const installationRouter = express.Router();
+const Project = require('../models/installationModels/project.model');
+const installationController = require('../controllers/installationController');
 
 //http://localhost:3000/api/v1/installation
 installationRouter.get('/test', installationController.testController);
+installationRouter.get('/projects/get', installationController.getProjects);
+installationRouter.get('/projects/get/:id', installationController.getProject);
+installationRouter.post('/projects/add', installationController.addProject);
+installationRouter.put('/projects/update/:id', installationController.updateProject);
+installationRouter.delete('/projects/delete/:id', installationController.deleteProject);
 
 
 
-module.exports = {installationRouter};
+module.exports = {installationRouter}
