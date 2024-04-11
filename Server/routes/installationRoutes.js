a1babc54b073dcb1bef85ebcdb441a997fae9039
gamithf <125730055+gamithf@users.noreply.github.com>
2024-04-11 06:11:59 +0530

    Checked the installation router

--- Server/routes/installationRoutes.js
+++ Server/routes/installationRoutes.js
@@ -2,6 +2,7 @@ const installationController = require('../controllers/installationController');
 const express = require('express');
 const installationRouter = express.Router();
 
+//http://localhost:3000/api/v1/installation
 installationRouter.get('/test', installationController.testController);
 
 
