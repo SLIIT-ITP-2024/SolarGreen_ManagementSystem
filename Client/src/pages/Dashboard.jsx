2dd8798df177ce081ab17e61351c8006ce152617
Mathumitha Gnanasekaran <102413599+Mathumitha-Gnanasekaran@users.noreply.github.com>
2024-04-16 11:55:22 +0530

    Page Dashboard
    connect

--- Client/src/pages/Dashboard.jsx
+++ Client/src/pages/Dashboard.jsx
@@ -1,11 +1,32 @@
 import ChartOne from "../components/Dashboard/ChartOne";
+import LineChart from "../components/Dashboard/LineChart/LineChart";
+import PieChart from "../components/Dashboard/PieChart/PieChart";
+import ProjectOverview from "../components/Dashboard/ProjectOverview/ProjectOverview";
+import StatisticChart from "../components/Dashboard/StatisticChart/StatisticChart";
+import TopCard from "../components/_Shared/topCard/TopCard";
 import WithLayout from "../hoc";
+import './Dashboard.scss';
+
+
 
 function Dashboard() {
     return ( 
-    <>
-        <ChartOne />
-    </> );
+      <div className="dashboard-outer">
+          <div className="top-section">
+            <TopCard title = "Total Revenue" descr = "$685.59" />
+            <TopCard title = "Activity" descr = "21 upcoming projects"/>
+          </div>
+
+          <div className="second-section">
+            <ProjectOverview />
+            <StatisticChart />
+          </div>
+          <div className="third-section">
+            <PieChart />
+            <LineChart />
+            </div>
+      </div> 
+    );
 }
 
 export default WithLayout(Dashboard);
