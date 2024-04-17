a7f0d732b19bc895fe85a6447db47a4ea24e2532
gamithf <125730055+gamithf@users.noreply.github.com>
2024-04-17 07:54:40 +0530

    Updated the location of Estimations.jsx

---
+++ Client/src/pages/InstallationManagement/Estimation/Estimations.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WithLayout from "../../../hoc/WithLayout";
+
+function Estimations() {
+  return (
+    <div>
+      <h3>Estimations</h3>
+    </div>
+  );
+}
+
+export default WithLayout(Estimations);
