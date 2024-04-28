924a93e1abe349674f18ebbbd27639da9a60a6e9
Mathumitha Gnanasekaran <102413599+Mathumitha-Gnanasekaran@users.noreply.github.com>
2024-04-28 09:16:25 +0530

    backend update

--- Server/controllers/customer&employeeController.js
+++ Server/controllers/customer&employeeController.js
@@ -1,7 +1,110 @@
+const Customer = require("../models/Customer");
 const Employee = require("../models/Employee");
 
 const testController = (req, res) => {
   res.send("employee and customer controller is working!");
+};
+
+const addCustomerControl = async (req, res) => {
+  try {
+    const { email, name, gender, phone, dob } = req.body;
+
+    console.log("req.body: ", req.body);
+
+    if (!email) return res.status(400).json({ errorMsg: "Email is required" });
+    if (!name) return res.status(400).json({ errorMsg: "Name is required" });
+    if (!gender)
+      return res.status(400).json({ errorMsg: "Gender is required" });
+    if (!dob)
+      return res.status(400).json({ errorMsg: "Starting date is required" });
+
+    // Check if the email is already registered
+    const existingUser = await Customer.findOne({ email });
+
+    if (existingUser) {
+      // Email already registered
+      return res.status(400).json({ errorMsg: "Email already exists" });
+    }
+
+    // Email not registered, proceed with user creation
+    const newEmployee = new Customer({
+      email,
+      name,
+      gender,
+      phone,
+      dob: new Date(dob),
+    });
+
+    await newEmployee.save();
+
+    return res.status(200).json({
+      successMsg: "Customers created successfully",
+      customer: newEmployee,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ errorMsg: "Error creating customers" });
+  }
+};
+
+const getAllCustomerControl = async (req, res) => {
+  try {
+    Customer.find().then((customers) => {
+      return res.status(200).json({
+        customers: customers,
+        successMsg: "Fetch all customers successfully",
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ errorMsg: "Error fetching customers" });
+  }
+};
+
+const editCustomerControl = async (req, res) => {
+  console.log("req.body: ", req.body._id);
+  if (!req.body._id) return res.status(500).json({ errorMsg: msg.noDataError });
+
+  Customer.findOneAndUpdate(
+    { _id: req.body._id },
+    { $set: { ...req.body, dob: new Date(req.body.dob) } },
+    { new: true }
+  )
+    .then((updatedCustomer) => {
+      console.log(updatedCustomer);
+      if (!updatedCustomer) {
+        return res.status(500).json({ errorMsg: "Error edit customer" });
+      }
+      return res.status(200).json({
+        successMsg: "Edit customer successfully",
+        customer: updatedCustomer,
+      });
+    })
+    .catch((error) => {
+      console.error("Update error:", error);
+      return res.status(500).json({ errorMsg: "Error edit customer" });
+    });
+};
+
+const deleteCustomerControl = async (req, res) => {
+  console.log("req.body delete: ", req.body._id);
+  if (!req.body._id) return res.status(500).json({ errorMsg: msg.noDataError });
+
+  Customer.findOneAndDelete({ _id: req.body._id })
+    .then((deletedCustomer) => {
+      console.log(deletedCustomer);
+      if (!deletedCustomer) {
+        return res.status(500).json({ errorMsg: "Error delete customer" });
+      }
+      return res.status(200).json({
+        successMsg: "delete customer successfully",
+        customer: deletedCustomer,
+      });
+    })
+    .catch((error) => {
+      console.error("Update error:", error);
+      return res.status(500).json({ errorMsg: "Error delete customer" });
+    });
 };
 
 const addEmployeeControl = async (req, res) => {
@@ -13,10 +116,13 @@ const addEmployeeControl = async (req, res) => {
 
     if (!email) return res.status(400).json({ errorMsg: "Email is required" });
     if (!name) return res.status(400).json({ errorMsg: "Name is required" });
-    if (!gender) return res.status(400).json({ errorMsg: "Gender is required" });
+    if (!gender)
+      return res.status(400).json({ errorMsg: "Gender is required" });
     if (!role) return res.status(400).json({ errorMsg: "Role is required" });
-    if (!startingDate) return res.status(400).json({ errorMsg: "Starting date is required" });
-    if (!personalDetail) return res.status(400).json({ errorMsg: "Personal date is required" });
+    if (!startingDate)
+      return res.status(400).json({ errorMsg: "Starting date is required" });
+    if (!personalDetail)
+      return res.status(400).json({ errorMsg: "Personal date is required" });
 
     // Check if the email is already registered
     const existingUser = await Employee.findOne({ email });
@@ -115,4 +221,8 @@ module.exports = {
   getAllEmployeeControl,
   editEmployeeControl,
   deleteEmployeeControl,
+  addCustomerControl,
+  getAllCustomerControl,
+  editCustomerControl,
+  deleteCustomerControl,
 };
