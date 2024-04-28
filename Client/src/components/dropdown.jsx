4dddcc4e232d31cc76f8867e3481b03a71169557
Mathumitha Gnanasekaran <102413599+Mathumitha-Gnanasekaran@users.noreply.github.com>
2024-04-28 06:37:41 +0530

    update

---
+++ Client/src/components/dropdown.jsx
@@ -0,0 +1,56 @@
+
+import React, { useState } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/24/solid';
+
+const DropdownMenu = ({ options, onSelect, placeholder, icon, className, value }) => {
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+
+  const handleItemClick = (value, label) => {
+    setSelectedOption({ value, label });
+    onSelect && onSelect({ value, label });
+  };
+
+  return (
+    <Menu as="div" className="relative inline-block text-left w-full">
+      <div>
+        <Menu.Button
+          className={flex ${className ? className : ''} justify-between items-center h-10 text-xs md:text-base w-full px-2 md:px-5 gap-x-5 rounded-md border-y-2 border-e-2 border-black hover:bg-gray-50}
+        >
+          {icon ? <img src={icon} alt="icon" /> : ''}
+          {value || selectedOption?.label || placeholder}
+          <ChevronDownIcon className="-mr-1 h-6 w-6 text-gray-400 border-2 rounded-md" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={React.Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-full origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          {options?.map((option, index) => (
+            <div key={index} className="py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <div
+                    className={${active ? 'bg-gray-100 text-gray-900' : 'text-gray-700'} block px-4 py-2 text-sm}
+                    onClick={() => handleItemClick(option?.value, option?.label)}
+                  >
+                    {option?.label}
+                  </div>
+                )}
+              </Menu.Item>
+            </div>
+          ))}
+        </Menu.Items>
+      </Transition>
+    </Menu>
+  );
+};
+
+export default DropdownMenu;
--- Server/controllers/customer&employeeController.js
+++ Server/controllers/customer&employeeController.js
@@ -1,27 +1,118 @@
-const customerEmployeeController = require("../controllers/customer&employeeController");
-const express = require("express");
-const customerEmployeeRouter = express.Router();
+const Employee = require("../models/Employee");
 
-customerEmployeeRouter.get("/test", customerEmployeeController.testController);
+const testController = (req, res) => {
+  res.send("employee and customer controller is working!");
+};
 
-customerEmployeeRouter.post(
-  "/add-employee",
-  customerEmployeeController.addEmployeeControl
-);
+const addEmployeeControl = async (req, res) => {
+  try {
+    const { email, name, gender, role, startingDate, personalDetail, phone } =
+      req.body;
 
-customerEmployeeRouter.post(
-  "/edit-employee",
-  customerEmployeeController.editEmployeeControl
-);
+    console.log("req.body: ", req.body);
 
-customerEmployeeRouter.post(
-  "/delete-employee",
-  customerEmployeeController.deleteEmployeeControl
-);
+    if (!email) return res.status(400).json({ errorMsg: "Email is required" });
+    if (!name) return res.status(400).json({ errorMsg: "Name is required" });
+    if (!gender) return res.status(400).json({ errorMsg: "Gender is required" });
+    if (!role) return res.status(400).json({ errorMsg: "Role is required" });
+    if (!startingDate) return res.status(400).json({ errorMsg: "Starting date is required" });
+    if (!personalDetail) return res.status(400).json({ errorMsg: "Personal date is required" });
 
-customerEmployeeRouter.get(
-  "/employee",
-  customerEmployeeController.getAllEmployeeControl
-);
+    // Check if the email is already registered
+    const existingUser = await Employee.findOne({ email });
 
-module.exports = { customerEmployeeRouter };
+    if (existingUser) {
+      // Email already registered
+      return res.status(400).json({ errorMsg: "Email already exists" });
+    }
+
+    // Email not registered, proceed with user creation
+    const newEmployee = new Employee({
+      email,
+      name,
+      gender,
+      role,
+      phone,
+      startingDate: new Date(startingDate),
+      personalDetail,
+    });
+
+    await newEmployee.save();
+
+    return res.status(200).json({
+      successMsg: "Employee created successfully",
+      employee: newEmployee,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ errorMsg: "Error creating employee" });
+  }
+};
+
+const getAllEmployeeControl = async (req, res) => {
+  try {
+    Employee.find().then((employees) => {
+      return res.status(200).json({
+        employees: employees,
+        successMsg: "Fetch all employee successfully",
+      });
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ errorMsg: "Error fetching employee" });
+  }
+};
+
+const editEmployeeControl = async (req, res) => {
+  console.log("req.body: ", req.body._id);
+  if (!req.body._id) return res.status(500).json({ errorMsg: msg.noDataError });
+
+  Employee.findOneAndUpdate(
+    { _id: req.body._id },
+    { $set: { ...req.body, startingDate: new Date(req.body.startingDate) } },
+    { new: true }
+  )
+    .then((updatedEmployee) => {
+      console.log(updatedEmployee);
+      if (!updatedEmployee) {
+        return res.status(500).json({ errorMsg: "Error edit employee" });
+      }
+      return res.status(200).json({
+        successMsg: "Edit employee successfully",
+        employee: updatedEmployee,
+      });
+    })
+    .catch((error) => {
+      console.error("Update error:", error);
+      return res.status(500).json({ errorMsg: "Error edit employee" });
+    });
+};
+
+const deleteEmployeeControl = async (req, res) => {
+  console.log("req.body delete: ", req.body._id);
+  if (!req.body._id) return res.status(500).json({ errorMsg: msg.noDataError });
+
+  Employee.findOneAndDelete({ _id: req.body._id })
+    .then((deletedEmployee) => {
+      console.log(deletedEmployee);
+      if (!deletedEmployee) {
+        return res.status(500).json({ errorMsg: "Error delete employee" });
+      }
+      return res.status(200).json({
+        successMsg: "delete employee successfully",
+        employee: deletedEmployee,
+      });
+    })
+    .catch((error) => {
+      console.error("Update error:", error);
+      return res.status(500).json({ errorMsg: "Error delete employee" });
+    });
+};
+
+module.exports = {
+  addEmployeeControl,
+  testController,
+  getAllEmployeeControl,
+  editEmployeeControl,
+  deleteEmployeeControl,
+};
