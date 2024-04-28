a15dbb9344767e40b907e784cd269d2e10d586df
Mathumitha Gnanasekaran <102413599+Mathumitha-Gnanasekaran@users.noreply.github.com>
2024-04-28 09:17:53 +0530

    Backend update

--- Server/routes/customer&employeeRoutes.js
+++ Server/routes/customer&employeeRoutes.js
@@ -1,3 +1,5 @@
+server -> customer&employerRoutes
+
 const customerEmployeeController = require("../controllers/customer&employeeController");
 const express = require("express");
 const customerEmployeeRouter = express.Router();
@@ -5,8 +7,43 @@ const customerEmployeeRouter = express.Router();
 customerEmployeeRouter.get("/test", customerEmployeeController.testController);
 
 customerEmployeeRouter.post(
-    "/add-employee",
-    customerEmployeeController.addEmployeeControl
+  "/add-employee",
+  customerEmployeeController.addEmployeeControl
+);
+
+customerEmployeeRouter.post(
+  "/edit-employee",
+  customerEmployeeController.editEmployeeControl
+);
+
+customerEmployeeRouter.post(
+  "/delete-employee",
+  customerEmployeeController.deleteEmployeeControl
+);
+
+customerEmployeeRouter.get(
+  "/employee",
+  customerEmployeeController.getAllEmployeeControl
+);
+
+customerEmployeeRouter.post(
+  "/add-customer",
+  customerEmployeeController.addCustomerControl
+);
+
+customerEmployeeRouter.post(
+  "/edit-customer",
+  customerEmployeeController.editCustomerControl
+);
+
+customerEmployeeRouter.post(
+  "/delete-customer",
+  customerEmployeeController.deleteCustomerControl
+);
+
+customerEmployeeRouter.get(
+  "/customer",
+  customerEmployeeController.getAllCustomerControl
 );
 
 module.exports = { customerEmployeeRouter };
